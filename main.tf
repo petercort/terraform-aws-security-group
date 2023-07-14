25f87ec7b09c45379e6284bc9ff362a9a4b7fdac
Peter Cort <petercort@gmail.com>
2023-07-14 18:55:34 -0400

    updating the index for the rules loop to be more unique, rather than index. Index would make it so every time the rules were reordered in some way they would have to be replaced, working similarly to count.

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
+terraform {
+  required_version = ">= 0.15.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
 locals {
   source_security_groups = toset(concat([for val in var.rules : val.security_group_name if val.security_group_name != ""]))
 }
+
 data "aws_security_group" "security_group" {
   depends_on = [resource.aws_security_group.create_security_groups]
   for_each = local.source_security_groups
@@ -18,7 +33,7 @@ resource "aws_security_group" "create_security_groups" {
 }
 
 resource "aws_security_group_rule" "create_sg_rules" {
-  for_each                 = { for index, rule in var.rules : index => rule }
+  for_each                 = { for index, rule in var.rules : format("%s-%s-%s-%s", rule.rule_type, rule.protocol, rule.from_port, rule.to_port) => rule }
   security_group_id        = aws_security_group.create_security_groups.id
   type                     = each.value.rule_type
   from_port                = each.value.from_port
