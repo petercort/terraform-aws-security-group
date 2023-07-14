9e2e968074ef333ff9bd787cb18397d5c4d4b232
Peter Cort <petercort@gmail.com>
2023-07-14 19:25:25 -0400

    running the entire rule up into the for block. hacky tolist to get it to handle empty lists. todo item would be to clean this up and require a unique identifier

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "create_security_groups" {
 }
 
 resource "aws_security_group_rule" "create_sg_rules" {
-  for_each                 = { for index, rule in var.rules : format("%s-%s-%s-%s", rule.rule_type, rule.protocol, rule.from_port, rule.to_port) => rule }
+  for_each                 = { for index, rule in var.rules : format("%s-%s-%s-%s-%s", rule.rule_type, rule.protocol, rule.from_port, rule.to_port, coalesce(element(concat(rule.ipv4_cidr_block, tolist([""])), 0), element(concat(rule.ipv6_cidr_block, tolist([""])), 0), rule.security_group_name)) => rule }
   security_group_id        = aws_security_group.create_security_groups.id
   type                     = each.value.rule_type
   from_port                = each.value.from_port
