e5315e0bc93edf85aea6428090b3c85e767e2f7f
Peter <petercort@gmail.com>
2024-09-20 08:25:38 -0400

    Update variables.tf
    add descriptions

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "name" {
   type = string
+  description = "Name of the security group to create"
 }
 
 variable "description" {
   type = string
+  description = "Description of what the security group is doing."
 }
 variable "vpc_id" {
   type = string
+  description = "ID of the VPC to attach the security group to."
 }
 
 variable "rules" {
@@ -21,4 +24,5 @@ variable "rules" {
     ipv6_cidr_block     = optional(list(string), [])
   }))
   default = []
+  description = "List of rule objects."
 }
